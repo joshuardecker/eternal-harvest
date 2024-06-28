ac8613bc3866e30ba3eeb66bc1bb69b5513c0e35
Joshua Decker <joshuardecker@proton.me>
2024-06-28 01:02:07 -0600

    Added a basic main menu, settings is still in the works

---
+++ HUD/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+## The main menu of the game. Only should be one at a time. Can be spawned
+## and deleted as much as needed. Settings are saved elsewhere.
+class_name MainMenu
+
+signal start
+signal exit
+
+func _on_start_pressed():
+	start.emit()
+	
+func _on_exit_pressed():
+	exit.emit()
