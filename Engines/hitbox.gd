1225ee70d08bbee5650a13ffb186b696473c5c79
Joshua Decker <joshuardecker@proton.me>
2024-06-27 20:50:38 -0600

    Hitboxes now have a damage attribute for when they hit something

--- Engines/hitbox.gd
+++ Engines/hitbox.gd
@@ -4,3 +4,6 @@ extends Area2D
 ## If the health component looked for just an area2d, unexpected behavior
 ## may occur.
 class_name Hitbox
+
+## When this hitbox is entered, how much damage does it do?
+@export var damage: float = 0
