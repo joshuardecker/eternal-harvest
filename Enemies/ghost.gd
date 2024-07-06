c0de1e94ea783652c5316b9d898075aa800430cf
Joshua Decker <joshuardecker@proton.me>
2024-07-06 02:45:25 -0600

    Formatted stuff at the top of the code, but importantly I added the ability for the ghost to properly caluculate new AI decsions when its time to, vs just fetch what its already decided to do

--- Enemies/ghost.gd
+++ Enemies/ghost.gd
@@ -6,6 +6,10 @@ extends CharacterBody2D
 ## Only chases the player.
 class_name Ghost
 
+# Kind of redundant, but this exists so I can link this signal to the game manager
+# and dont have to hook the HealthEngine to the game manager.
+signal dead
+
 @export var speed: float = 20
 @export_enum("simple_movement", "ai_movement") var movement_type: String = "simple_movement"
 
@@ -13,10 +17,12 @@ class_name Ghost
 @onready var hitbox_shape = $Hitbox/CollisionShape
 @onready var ghost_ai: GhostAI = $GhostAI
 
-# Kind of redundant, but this exists so I can link this signal to the game manager.
-signal dead
-
 var player: Player
+
+# How long in seconds since the AIEngine made a new decision on what to do.
+var last_ai_decision_time: float
+## How often in seconds the AI engine should make a new decision on what to do.
+@export var ai_decision_time_freq: float = 1
 
 func _ready():
 	player = get_tree().get_first_node_in_group("Player")
@@ -37,8 +43,8 @@ func _ready():
 	# When the ghost dies, tell the hud to add one to the kill counter.
 	connect("dead", hud.update_kill_amount)
 
-func _process(_delta):
-	move()
+func _process(delta: float):
+	move(delta)
 
 func despawn():
 	queue_free()
@@ -65,12 +71,12 @@ func _on_health_engine_is_dead():
 	# No errors, no nothing.
 	call_deferred("fancy_death")
 	
-func move():
+func move(delta: float):
 	match movement_type:
 		"simple_movement":
 			simple_movement()
 		"ai_movement":
-			ai_movement()
+			ai_movement(delta)
 		_:
 			push_error("This ghost is using a movement engine that doesnt exist!")
 
@@ -84,10 +90,22 @@ func simple_movement():
 	animation_tree.set("parameters/Move/blend_position", velocity)
 
 # Takes the feedback from the GhostAI engine and performs the action.
-func ai_movement():
-	match ghost_ai.calculate_decision():
+func ai_movement(delta: float):
+	# Remember a total of how long since the last ai decision was made.
+	last_ai_decision_time += delta
+	
+	# If its been long enough, calculate a new decison on what to do.
+	# This doesnt mean behavior will change since the same answer can be
+	# given again by calculate_decision, but it could change.
+	if last_ai_decision_time > ai_decision_time_freq:
+		ghost_ai.calculate_decision()
+		
+		# Reset time since a decision was made.
+		last_ai_decision_time = 0
+	
+	match ghost_ai.get_decision():
 		GhostAI.decision.CHASE_PLAYER:
 			# TODO: make the movement more advanced.
 			simple_movement()
 		_:
-			print("I tried to ", ghost_ai.decision)
+			print("I tried to ", ghost_ai.get_decision())
