21fae85e516db6b7134faef0bc31d6badb3aaa00
Joshua Decker <joshuardecker@proton.me>
2024-07-08 00:45:19 -0600

    Uses the game manager to find stats

--- UI/hud.gd
+++ UI/hud.gd
@@ -5,16 +5,30 @@ class_name HUD
 
 @onready var wave_num_label = $CenterContainer/HBoxContainer/WaveNumber
 @onready var player_health_label = $CenterContainer/HBoxContainer/PlayerHealth
-@onready var kill_amount_label = $CenterContainer/HBoxContainer/KillAmount
+@onready var score_label = $CenterContainer/HBoxContainer/Score
 
 var wave_num: int = 0
-var kill_amount: int = 0
+var score: int = 0
+
+var stats_manager: StatsManager
 
 func _ready():
+	stats_manager = get_tree().get_first_node_in_group("StatsManager")
+	
+	# If the stats manager could not be loaded.
+	if not stats_manager:
+		push_error("The HUD could not find the stats manager!")
+		
+	stats_manager.connect("new_player_score", update_score)
+	stats_manager.connect("new_player_health", update_player_health)
+	# TODO: add currency part to hud
+	#stats_manager.connect("new_player_currency")
+	
 	# Load a ghost spawner, that way the hud has a source of information
 	# of when the next wave has spawned.
 	var some_spawner = get_tree().get_first_node_in_group("GhostSpawner")
 	
+	# If a ghost spawner could not be found.
 	if not some_spawner:
 		push_error("The HUD could not load a ghost spawner!")
 		
@@ -29,15 +43,15 @@ func update_player_health(amount: float):
 	player_health_label.text = "Health: " + str(floor(amount))
 	
 # Increases the displayed kill counter by 1.
-func update_kill_amount():
-	kill_amount += 1
-	kill_amount_label.text = "Kills: " + str(kill_amount)
+func update_score(amount: int):
+	score = amount
+	score_label.text = "Score: " + str(score)
 	
 # Full reset of whats saved and displayed.
 func reset():
 	wave_num = 0
-	kill_amount = 0
+	score = 0
 	
 	wave_num_label.text = "Wave: Soon!"
 	player_health_label.text = "Health: 100"
-	kill_amount_label.text = "Kills: 0"
+	score_label.text = "Score: 0"
