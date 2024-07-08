df8d585bc34ee8f1921ea81311a29d8e556b0421
Joshua Decker <joshuardecker@proton.me>
2024-07-08 00:07:44 -0600

    Added the basics to a global stats manager

---
+++ Managers/stats_manager.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+## A simple manager that remembers stats of important entities.
+## Examples of important entities are the player and any bosses.
+class_name StatsManager
+
+# Player stats:
+var player_kills: int
+var player_score: int
+var player_health: float
+var player_currency: int
