b083d86e694e40c13339341b1c0312419419c1b6
Joshua Decker <joshuardecker@proton.me>
2024-08-04 22:42:53 -0600

    Much nicer logo and buttons

--- UI/MainMenu/main_menu.gd
+++ UI/MainMenu/main_menu.gd
@@ -8,6 +8,15 @@ signal start
 signal settings
 signal exit
 
+# Load this to get its initial y coord.
+# That way if I move it in the editor,
+# I dont have to change the code.
+@onready var game_title: CenterContainer = $CenterContainer
+@onready var button_hover: AudioStreamPlayer = $ButtonHover
+
+var y_cord: float
+var internal_timer: float
+
 func _on_start_pressed():
 	start.emit()
 
@@ -16,3 +25,23 @@ func _on_settings_pressed():
 
 func _on_exit_pressed():
 	exit.emit()
+
+func _on_start_mouse_entered():
+	button_hover.play()
+
+func _on_settings_mouse_entered():
+	button_hover.play()
+
+func _on_exit_mouse_entered():
+	button_hover.play()
+
+func _ready():
+	y_cord = game_title.global_position.y
+
+func _process(delta: float):
+	internal_timer += delta
+	
+	game_title.global_position.y = y_cord + custom_sin(internal_timer)
+
+func custom_sin(num: float) -> float:
+	return sin(num) * 15
